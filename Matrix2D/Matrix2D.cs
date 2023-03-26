d2ce2c07c22b8e9f342edc18a780f1a62db96b62
Bartłomiej Bobowski <bartlomiej.bobowski@microsoft.wsei.edu.pl>
2023-03-26 13:21:27 +0200

    init

---
+++ Matrix2D/Matrix2D.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace Matrix2D
+{
+    public class Matrix2D
+    {
+        public int A { get; init; }
+        public int B { get; init; }
+        public int C { get; init; }
+        public int D { get; init; }
+
+        public Matrix2D(int a, int b, int c, int d)
+        {
+            A = a;
+            B = b;
+            C = c;
+            D = d;
+        }
+
+        public Matirx() : this(1, 0, 0, 1) {}
+
+
+    }
+}
